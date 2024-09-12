c2585089b9a5247d5aa5c2f513545b35f9cd5876
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2024-09-12 23:48:47 +0100

    api-fix

--- Client/src/pages/CreatePost.tsx
+++ Client/src/pages/CreatePost.tsx
@@ -45,7 +45,7 @@ const CreatePost = () => {
       try {
         setgenratingImg(true);
         setdisable(true)
-        const { data } = await axios.post("http://localhost:3000/api/v1/octo", { prompt: form.prompt })
+        const { data } = await axios.post("https://dall-e-image-generator-8csh.onrender.com/api/v1/octo", { prompt: form.prompt })
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.url}` })
         setgenratingImg(false);
         setdisable(false)
